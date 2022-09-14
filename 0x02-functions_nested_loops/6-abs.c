ef60e807440bdb6efe9f5c066113673c2828daaa
ianugaji <ianugaji380@gmail.com>
2022-09-14 02:02:00 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @n: integer to make absolute
+ * Return: int or int time negative
+ *
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
