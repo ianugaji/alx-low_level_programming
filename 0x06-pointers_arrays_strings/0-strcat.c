8777e353d13614316f4989c0cb6c58af2cbdcdb8
ianugaji <ianugaji380@gmail.com>
2022-09-22 11:15:02 -0700

    Task 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
  * @dest: String that will be appended
- * @src: String to be concatenatedupon
+ * @src: String to be concatenated upon
  *
  * Return: returns poiner to @dest
  */
